952bbbabb7db05e52add09c8be80fd812d9fff7e
PaulBecerra <paul.becerra216567@potros.itson.edu.mx>
2022-03-07 16:48:05 -0700

    activity movie design

---
+++ app/src/main/java/becerra/paul/peliculas/MovieActivity.kt
@@ -0,0 +1,11 @@
+package becerra.paul.peliculas
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MovieActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_movie)
+    }
+}
