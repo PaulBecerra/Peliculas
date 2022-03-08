b6482d135f0f01cbe454e234ccb00620c39ef2ee
PaulBecerra <paul.becerra216567@potros.itson.edu.mx>
2022-03-07 18:35:11 -0700

    added movies menu item

--- app/src/main/java/becerra/paul/peliculas/ui/home/HomeFragment.kt
+++ app/src/main/java/becerra/paul/peliculas/ui/home/HomeFragment.kt
@@ -4,13 +4,17 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ListView
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import becerra.paul.peliculas.Movie
+import becerra.paul.peliculas.MovieAdapter
+import becerra.paul.peliculas.R
 import becerra.paul.peliculas.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
-
+    var movies: ArrayList<Movie> = ArrayList()
     private var _binding: FragmentHomeBinding? = null
 
     // This property is only valid between onCreateView and
@@ -28,15 +32,36 @@ class HomeFragment : Fragment() {
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
+        /**
         val textView: TextView = binding.textHome
         homeViewModel.text.observe(viewLifecycleOwner) {
             textView.text = it
-        }
+        }*/
+
+        addMockMovies()
+
+        var adapter = MovieAdapter(root.context, movies)
+
+        var listView: ListView = binding.listViewMovies
+
+        listView.adapter = adapter
+
         return root
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    private fun addMockMovies(){
+        val movie1 = Movie(1, getString(R.string.movie1), getString(R.string.movie1_desc), 120, R.drawable.jackass)
+        val movie2 = Movie(2,  getString(R.string.movie2), getString(R.string.movie2_desc), 60, R.drawable.jugarencasa)
+        val movie3 = Movie(3,  getString(R.string.movie3), getString(R.string.movie3_desc), 100, R.drawable.monstermania)
+        val movie4 = Movie(4,  getString(R.string.movie4), getString(R.string.movie4_desc), 110, R.drawable.queenpins)
+        val movie5 = Movie(5,  getString(R.string.movie5), getString(R.string.movie5_desc), 90, R.drawable.quienesquien)
+        val movie6 = Movie(6,  getString(R.string.movie6), getString(R.string.movie6_desc), 75, R.drawable.unbuclesinfin)
+        movies.addAll(listOf(movie1, movie2,movie3, movie4, movie5, movie6))
+
     }
 }
